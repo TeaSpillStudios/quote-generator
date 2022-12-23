310d8c8b5a05177701a68b4b75cc22dfbf013014
TeaSpillStudios <teaspillstudios@proton.me>
2022-12-23 00:46:49 +0000

    Add basic structure to crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,8 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+use xdg::BaseDirectories;
+
+pub struct QuoteGenerator {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
 }
