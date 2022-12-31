e45801410df4fad881542b9f3204e1238994527d
TeaSpillStudios <teaspillstudios@proton.me>
2022-12-31 17:31:42 +0000

    Add a load function

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-use ron::to_string;
+use ron::{from_str, to_string};
 use serde::{Deserialize, Serialize};
 use std::collections::HashMap;
 use std::{
@@ -27,6 +27,28 @@ impl QuoteGenerator {
 
         conf.write_all(data.as_bytes())
             .expect("Failed to write data.")
+    }
+
+    pub fn load_from_file() -> Self {
+        let base_dirs = BaseDirectories::with_prefix("quote_generator").unwrap();
+
+        let path = Path::new("").join(base_dirs.get_data_home());
+
+        let mut data = String::new();
+
+        if path.exists() {
+            let mut data_file = File::open(path).expect("Failed to open the data file.");
+
+            data_file
+                .read_to_string(&mut data)
+                .expect("Failed to read the data.");
+
+            from_str(&data).unwrap()
+        } else {
+            Self {
+                quotes: HashMap::new(),
+            }
+        }
     }
 }
 
